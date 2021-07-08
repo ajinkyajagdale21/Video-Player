e1fa29441bf578277aff67056fba0abf6811ba0f
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2021-07-08 19:39:49 +0530

    update: initial setup

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = () => {
+    return [
+      {
+        playId: "wY6UyatwVTA",
+        thumbnail: "",
+        image:
+          "",
+  
+        title:
+          "EURO 2020 | SEMI FINALS - 1 | Spain vs Italy",
+        channel: "Sony Sports India",
+        views: 0,
+        timestamp: "July 7 2021",
+      }
+    
+    ]
+}
